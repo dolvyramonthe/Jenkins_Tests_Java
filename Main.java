dc8a75a118f8d71a3a7c3b4732211f52b2c78fec
Roseline MONTHE WOUENDEU <102028926+dolvyramonthe@users.noreply.github.com>
2025-06-11 10:51:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
+ 
+java 
 class Main {
   public static void main(String[] args) {
-     if(args[(args.length-1)].equals("+")){
-        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-      }
-      else if(args[(args.length-1)].equals("-")){
-        System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
-      }
-      else {System.exit(-1);}
+    if(args[(args.length-1)].equals("+")){
+      System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
     }
+    else if(args[(args.length-1)].equals("-")){
+      System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
+    }
+    else if(args[(args.length-1)].equals("*")){
+      System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
+    }
+    else if(args[(args.length-1)].equals("/")){
+      System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
+    }
+    else {
+      System.exit(-1);
+    }
+  }
 }
